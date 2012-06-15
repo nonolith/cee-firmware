a6f3cd3838df61d2ab13a6012c7c1be213111894
Kevin Mehall <km@kevinmehall.net>
2012-06-15 10:37:00 -0600

    update.py: increase delay to play nicer with connect.

--- update.py
+++ update.py
@@ -13,7 +13,7 @@ try:
 	exit(0)
 except IOError:
 	usb.core.find(idVendor=0x59e3, idProduct=0xCEE1).ctrl_transfer(0x40|0x80, 0xBB, 0, 0, 1)
-	time.sleep(1)
+	time.sleep(2)
 	b = Bootloader()
 	b.handle_args(sys.argv)
 	exit(0)
